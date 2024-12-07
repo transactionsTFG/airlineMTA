762cff672a5a300a60da870220e7aff8faa6440f
josevillacres46 <jvillacr@ucm.es>
2024-12-07 21:10:46 +0100

    FEAT: Add 2 Persistance.xml retrocompability with JUnit and EclipseLink

--- src/main/java/integration/entitymanagerfactory/SingletonEntityManagerFactory.java
+++ src/main/java/integration/entitymanagerfactory/SingletonEntityManagerFactory.java
@@ -1,17 +1,18 @@
 package integration.entitymanagerfactory;
 
+
+import integration.environment.PersistanceConfigFile;
 import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.Persistence;
 
 public class SingletonEntityManagerFactory {
     private static EntityManagerFactory instance;
-	private static final String NAME_PERSISTANCE = "MtaAirline";
     private SingletonEntityManagerFactory(){}
 
     public static synchronized EntityManagerFactory getInstance() {
 		if (instance == null) 
-			instance = Persistence.createEntityManagerFactory(NAME_PERSISTANCE);
-    
+			instance = Persistence.createEntityManagerFactory(new PersistanceConfigFile().getNameFile());
+	
 		return instance;
 	}
 
