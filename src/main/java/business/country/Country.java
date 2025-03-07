13863dc7a60d51f2736b6c1d7933968023914286
danielfdez17 <daniel17dev@gmail.com>
2025-03-07 18:45:24 +0100

    FIX: añadido id autoincrement a Country.java

--- src/main/java/business/country/Country.java
+++ src/main/java/business/country/Country.java
@@ -1,6 +1,8 @@
 package business.country;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Version;
 
@@ -16,6 +18,7 @@ import lombok.Setter;
 @NoArgsConstructor
 public class Country {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
     private String name;
     private boolean active;
